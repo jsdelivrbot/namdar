153f5312dae7dffe8ad8c58415134b239b326791
Horvo <horvo@asperasoft.com>
2016-09-16 13:57:20 -0700

    removed route to hello index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'welcome/index'
-
-  get 'hello/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
