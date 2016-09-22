32faba857c6400e40b5fff53a11ffbcdebc3df54
Horvo <horvo@asperasoft.com>
2016-09-22 15:10:04 -0700

    Fix typo in route definition

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
-  delete 'logout' => 'session#destroy'
+  delete 'logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
