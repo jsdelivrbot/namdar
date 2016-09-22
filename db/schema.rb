c9d93ad19e10daf46ce8b1fa491b873ab73ab6b4
Horvo <horvo@asperasoft.com>
2016-09-22 11:30:32 -0700

    Removed column :password from User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922163148) do
+ActiveRecord::Schema.define(version: 20160922182509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 20160922163148) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        limit: 40
-    t.string   "password"
     t.string   "email"
     t.string   "group"
     t.datetime "created_at",                 null: false
