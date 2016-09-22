cae9a1e55c38c0af42b7856a84f2b73223759829
Horvo <horvo@asperasoft.com>
2016-09-22 13:47:28 -0700

    Using flash to show error message when login failed.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -13,10 +13,13 @@ class WelcomeController < ApplicationController
     if user
       session[:user_id] = user.id
       session[:user_group] = user.group
+      flash[:sucess] = "Logged in as #{user.username}!"
 
       redirect_to articles_path
     else
       session[:user_group] = 'user'
+      flash[:danger] = 'Invalid username or password'
+
       redirect_to :login
     end
   end
