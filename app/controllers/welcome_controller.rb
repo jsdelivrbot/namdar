ee0548540875e1e4413390044dc2df13f3cf6aa1
Horvo <horvo@asperasoft.com>
2016-09-22 09:34:56 -0700

    Added temporary login action and routes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def login
+    render :login
+  end
+
+  def authenticate
+    redirect_to articles_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   end
 
   root 'welcome#index'
+
+  get 'login' => 'welcome#login'
+  post 'login' => 'welcome#authenticate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
