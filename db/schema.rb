262d6bc3ad662e366fa04697921255882a40d74e
Horvo <horvo@asperasoft.com>
2016-09-22 09:36:08 -0700

    Added password digest column for User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922052329) do
+ActiveRecord::Schema.define(version: 20160922163148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,12 +32,13 @@ ActiveRecord::Schema.define(version: 20160922052329) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",   limit: 40
+    t.string   "username",        limit: 40
     t.string   "password"
     t.string   "email"
     t.string   "group"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.string   "password_digest"
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
