d2d2327cc61ffaf5b0f8785c91e4ced68589a261
Horvo <horvo@asperasoft.com>
2016-09-21 22:27:10 -0700

    Generated user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919181734) do
+ActiveRecord::Schema.define(version: 20160922052329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 20160919181734) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",   limit: 40
+    t.string   "password"
+    t.string   "email"
+    t.string   "group"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
   add_foreign_key "comments", "articles"
