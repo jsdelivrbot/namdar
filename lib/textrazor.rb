6c4a23ef1a21ef36fab6ee3ae202a77dd2fbd569
Horvo <horvo@asperasoft.com>
2016-09-27 12:11:18 -0700

    created api communication class

---
+++ lib/textrazor.rb
@@ -0,0 +1,30 @@
+module Textrazor
+  require 'net/http'
+  require 'json'
+
+  class Client
+    API_KEY = Rails.application.secrets.textrazor_api_key
+    REQUEST_HEADER = {'x-textrazor-key' => API_KEY}
+    ENDPOINT = 'http://api.textrazor.com'
+
+    @last_response
+
+    def get_topics(article_body)
+      params = 'extractors=topics&text=' + article_body
+      response = do_request params
+
+      response["response"]["topics"]
+    end
+
+    private
+
+    def do_request(params)
+      uri = URI.parse(ENDPOINT)
+      http = Net::HTTP.new(uri.host, uri.port)
+
+      response = http.post(uri, params, REQUEST_HEADER)
+
+      JSON.parse(response.body)
+    end
+  end
+end
